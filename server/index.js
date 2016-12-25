260dfb81b935a2c5b697016f686cf7c612fd965e
Vincent Le <vinnyoodles@gmail.com>
2016-12-24 21:31:09 -0500

    Save messages and allow server to talk

--- server/index.js
+++ server/index.js
@@ -2,27 +2,92 @@ var express = require('express');
 var http = require('http')
 var socketio = require('socket.io');
 var mongojs = require('mongojs');
-var ObjectID = mongojs.ObjectID;
 
+var ObjectID = mongojs.ObjectID;
 var db = mongojs(process.env.MONGO_URL || 'mongodb://localhost:27017/local');
 var app = express();
 var server = http.Server(app);
 var websocket = socketio(server);
+server.listen(3000, () => console.log('listening on *:3000'));
+
+// Mapping objects to easily map sockets and users.
 var clients = {};
+var users = {};
 
-websocket.on('connection', function(socket){
-  clients[socket.id] = socket;
-  var userId = new ObjectID();
-  socket.emit('userId', userId);
-  socket.on('message', function(message) {
-    saveAndSendMessage(message, userId);
-  });
+// This represents a unique chatroom.
+// For this example purpose, there is only one chatroom;
+var chatId = 1;
+
+websocket.on('connection', (socket) => {
+    clients[socket.id] = socket;
+    socket.on('userJoined', (userId) => onUserJoined(userId, socket));
+    socket.on('message', (message) => onMessageReceived(message, socket));
 });
 
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-function saveAndSendMessage(message, senderId) {
-  console.log(message);
+// Event listeners.
+// When a user joins the chatroom.
+function onUserJoined(userId, socket) {
+  try {
+    // The userId is null for new users.
+    if (!userId) {
+      var user = db.collection('users').insert({}, (err, user) => {
+        socket.emit('userJoined', user._id);
+        users[socket.id] = user._id;
+        _sendExistingMessages(socket);
+      });
+    } else {
+      users[socket.id] = userId;
+      _sendExistingMessages(socket);
+    }
+  } catch(err) {
+    console.err(err);
+  }
 }
+
+// When a user sends a message in the chatroom.
+function onMessageReceived(message, senderSocket) {
+  var userId = users[senderSocket.id];
+  // Safety check.
+  if (!userId) return;
+
+  _sendAndSaveMessage(message, senderSocket);
+}
+
+// Helper functions.
+// Send the pre-existing messages to the user that just joined.
+function _sendExistingMessages(socket) {
+  var messages = db.collection('messages')
+    .find({ chatId })
+    .sort({ createdAt: 1 })
+    .toArray((err, messages) => {
+      // If there aren't any messages, then return.
+      if (!messages.length) return;
+      socket.emit('message', messages.reverse());
+  });
+}
+
+// Save the message to the db and send all sockets but the sender.
+function _sendAndSaveMessage(message, socket, fromServer) {
+  var messageData = {
+    text: message.text,
+    user: message.user,
+    createdAt: new Date(message.createdAt),
+    chatId: chatId
+  };
+
+  db.collection('messages').insert(messageData, (err, message) => {
+    // If the message is from the server, then send to everyone.
+    var emitter = fromServer ? websocket : socket.broadcast;
+    emitter.emit('message', [message]);
+  });
+}
+
+// Allow the server to participate in the chatroom through stdin.
+var stdin = process.openStdin();
+stdin.addListener('data', function(d) {
+  _sendAndSaveMessage({
+    text: d.toString().trim(),
+    createdAt: new Date(),
+    user: { _id: 'robot' }
+  }, null /* no socket */, true /* send from server */);
+});
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import { View, Text, AsyncStorage } from 'react-native';
 import SocketIOClient from 'socket.io-client';
 import { GiftedChat } from 'react-native-gifted-chat';
 
+const USER_ID = '@userId';
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -10,31 +12,55 @@ class Main extends React.Component {
       messages: [],
       userId: null
     };
-    this.socket = SocketIOClient('http://localhost:3000');
-    this.socket.on('message', this.onReceivedMessage);
-    this.socket.on('userId', (userId) => this.setState({ userId }));
 
+    this.determineUser = this.determineUser.bind(this);
     this.onReceivedMessage = this.onReceivedMessage.bind(this);
     this.onSend = this.onSend.bind(this);
+    this._storeMessages = this._storeMessages.bind(this);
+
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.on('message', this.onReceivedMessage);
+    this.determineUser();
   }
 
-  componentWillMount() {
-    // Perform prefetching of data.
+  /**
+   * When a user joins the chatroom, check if they are an existing user.
+   * If they aren't, then ask the server for a userId.
+   * Set the userId to the component's state.
+   */
+  determineUser() {
+    AsyncStorage.getItem(USER_ID)
+      .then((userId) => {
+        // If there isn't a stored userId, then fetch one from the server.
+        if (!userId) {
+          this.socket.emit('userJoined', null);
+          this.socket.on('userJoined', (userId) => {
+            AsyncStorage.setItem(USER_ID, userId);
+            this.setState({ userId });
+          });
+        } else {
+          this.socket.emit('userJoined', userId);
+          this.setState({ userId });
+        }
+      })
+      .catch((e) => alert(e));
   }
 
   // Event listeners
-
-  onReceivedMessage(message) {
-    this.onSend([message]);
+  /**
+   * When the server sends a message to this.
+   */
+  onReceivedMessage(messages) {
+    this._storeMessages(messages);
   }
 
+  /**
+   * When a message is sent, send the message to the server
+   * and store it in this component's state.
+   */
   onSend(messages=[]) {
     this.socket.emit('message', messages[0]);
-    this.setState((previousState) => {
-      return {
-        messages: GiftedChat.append(previousState.messages, messages),
-      };
-    });
+    this._storeMessages(messages);
   }
 
   render() {
@@ -47,6 +73,15 @@ class Main extends React.Component {
         user={user}
       />
     );
+  }
+
+  // Helper functions
+  _storeMessages(messages) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
   }
 }
 
